16418dcc6c0df2c1085fe02b1c76bf00810a5395
CAPpython <Geetanjalia1995@gmail.com>
2018-08-16 11:56:52 +0530

    class

---
+++ 3rd day.py
@@ -0,0 +1,46 @@
+
+'''
+a=len("python")
+
+print(a)
+
+
+b= len([1,2,34,5,6,67,78,8])
+print(b)
+
+ 
+a='A' in "Anjali"  #membership in function
+print(a)
+
+b='n' in "anjali"
+print(b)
+
+c='J' in "Anjali"
+print(c)
+
+z="Anjali" in ['Anjali','adarsh','chanda']
+print(z)
+
+
+name=input("Your name please: ")
+print("hello i'm Anjali",name)
+
+A=10
+if A>2:
+    print("i'm anjli")
+
+    '''
+A=5
+B=5
+if A>B:
+    print('A is greater')
+elif B>A:
+    print('b is greater')
+else:
+    print('both same')
+    
+
+
+
+
+    
