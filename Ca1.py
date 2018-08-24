eda411e7a6cc9f04f3fc288992bfd6e1cdd0fecb
CAPpython <Geetanjalia1995@gmail.com>
2018-08-24 14:10:39 +0530

    CA1

---
+++ Ca1.py
@@ -0,0 +1,21 @@
+>>> myname= "Ms. Geetanjali Saroha"
+>>> myname
+'Ms. Geetanjali Saroha'
+1.)
+>>> first_name="Ms."
+>>> middle_name="Geetanjali"
+>>> last_name="Saroha"
+first_name+' ' +middle_name+' '+last_name
+'Ms. Geetanjali Saroha'
+
+
+2.)reverse=myname[::-1]
+>>> reverse
+'ahoraS ilajnateeG .sM'
+
+
+3.)>>> myname[0:4]
+'Mr. '
+4.)>>> myname[-3:]
+'oha'
+5.)
